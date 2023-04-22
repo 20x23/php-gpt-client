0c90653cfd7eba6beb535109e59cead9a4f824fe
thomas <thomas@trade-ix.de>
2023-04-23 00:24:24 +0200

    Name in message is not required

--- src/Api/Message.php
+++ src/Api/Message.php
@@ -5,19 +5,24 @@ namespace TeeV\GptClient\Api;
 class Message
 {
   public function __construct(
-    private readonly Role   $role,
-    private readonly string $content,
-    private readonly string $name
+    private readonly Role    $role,
+    private readonly string  $content,
+    private readonly ?string $name
   )
   {
   }
 
   public function getMessage(): array
   {
-    return [
+    $message = [
       'role' => $this->role->value,
-      'content' => $this->content,
-      'name' => $this->name
+      'content' => $this->content
     ];
+
+    if (null !== $this->name) {
+      $message['name'] = $this->name;
+    }
+
+    return $message;
   }
 }
