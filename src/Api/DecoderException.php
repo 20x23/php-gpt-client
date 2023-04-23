0d65c62fe2ad72849d5fd459d354b2a2a33a2cc4
thomas <thomas@trade-ix.de>
2023-04-23 14:43:16 +0200

    Improve typing

---
+++ src/Api/DecoderException.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace TeeV\GptClient\Api;
+
+use Exception;
+
+/** Unable to decode response-message */
+final class DecoderException extends Exception
+{
+
+}
